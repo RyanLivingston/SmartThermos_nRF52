511846a80066671bfaa6e9d9e25a30131411a9e8
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2019-03-20 12:41:32 -0400

    Initial Commit

---
+++ SmartThermos_nRF52/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  SmartThermos_nRF52
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project SmartThermos_nRF52
+  Created by Ryan Livingston on 3/20/19
+  Copyright © 2019 Ryan Livingston
+  Licence CC = BY SA NC
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei Vilo, 2010-2019
+  All rights reserved
+  https://embedXcode.weebly.com
+
---
+++ SmartThermos_nRF52/SmartThermos_nRF52.ino
@@ -0,0 +1,109 @@
+///
+/// @mainpage	SmartThermos_nRF52
+///
+/// @details	Description of the project
+/// @n
+/// @n
+/// @n @a		Developed with [embedXcode+](https://embedXcode.weebly.com)
+///
+/// @author		Ryan Livingston
+/// @author		Ryan Livingston
+/// @date		3/20/19 12:41 PM
+/// @version	<#version#>
+///
+/// @copyright	(c) Ryan Livingston, 2019
+/// @copyright	CC = BY SA NC
+///
+/// @see		ReadMe.txt for references
+///
+
+
+///
+/// @file		SmartThermos_nRF52.ino
+/// @brief		Main sketch
+///
+/// @details	<#details#>
+/// @n @a		Developed with [embedXcode+](https://embedXcode.weebly.com)
+///
+/// @author		Ryan Livingston
+/// @author		Ryan Livingston
+/// @date		3/20/19 12:41 PM
+/// @version	<#version#>
+///
+/// @copyright	(c) Ryan Livingston, 2019
+/// @copyright	CC = BY SA NC
+///
+/// @see		ReadMe.txt for references
+/// @n
+///
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"
+#elif defined(ROBOTIS) // Robotis specific
+#include "libpandora_types.h"
+#include "pandora.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#include "Arduino.h"
+#elif defined(RFDUINO) // RFduino specific
+#include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#include "application.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+// !!! Help http://bit.ly/2CL22Qp
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+
+}
+
+// Add loop code
+void loop()
+{
+
+}
